234450058bd5ca47e59e0a8a5208a05255c8aab4
imli700 <imlijangba@gmail.com>
2023-12-06 21:14:10 +0530

    Add Neorg plugin

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neorg/neorg",
+  lazy = false,
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              top = "~/Documents/zettelkasten-notes/top/",
+              communicationSkills = "~/Documents/zettelkasten-notes/2-semester/communication-skills/",
+              problemSolvingAndProgramming = "~/Documents/zettelkasten-notes/2-semester/problem-solving-and-programming/",
+              computerOrganisationAndAssemblyLanguageProgramming = "~/Documents/zettelkasten-notes/2-semester/computer-organisation-and-assembly-language-programming/",
+            },
+            default_workspace = "top",
+          },
+        },
+      },
+    })
+  end,
+}
