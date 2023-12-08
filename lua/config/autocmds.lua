d7366066f015417ab3db070c0e3bfe0e542f0662
imli700 <imlijangba@gmail.com>
2023-12-08 06:56:21 +0530

    Make indentation consistent for wraps
    I made the options.lua file cleaner then added option to make sure
    wrapped lines have equal indentation to their original lines. Also moved
    the autocmd for setting wrap and spell for norg files from options.lua
    to autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set local options for norg files
+vim.api.nvim_command("autocmd FileType norg setlocal spell wrap")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.linebreak = true
 
-vim.cmd([[
-  augroup SpellAndWrap
-    autocmd!
-    autocmd FileType norg setlocal spell wrap
-  augroup END
-]])
+local opt = vim.opt
+
+opt.linebreak = true -- Makes sure wraps don't break in-between words
+opt.breakindent = true -- Make sure indentation of wrapped lined matches indentation of oringal line
