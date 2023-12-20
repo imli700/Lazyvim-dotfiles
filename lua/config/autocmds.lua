b448f0e0016b068a980c30508815d82152e08406
imli700 <imlijangba@gmail.com>
2023-12-20 15:47:40 +0530

    Add autocmds to remember folds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,12 @@ vim.cmd([[
     autocmd FileType norg setlocal formatoptions-=cro
   augroup END
 ]])
+
+-- Remember folds
+vim.cmd([[
+  augroup remember_folds
+    autocmd!
+    autocmd BufWinLeave ?* mkview
+    autocmd BufWinEnter ?* silent! loadview
+  augroup END
+]])
