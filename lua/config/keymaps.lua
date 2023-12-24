b846517d3b999bd812586ee4b923ee24e8e45876
imli700 <imlijangba@gmail.com>
2023-12-24 14:00:56 +0530

    Set 'n' and 'N' to their default behaviors
    LazyVim's 'n' and 'N', for backwards search, worked completely different
    from how default Neovim's worked. 'n' is supposed to continue looking
    backwards but searched forwards instead. Messed me up.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete the existing mappings for 'n' and 'N'
+vim.keymap.del("n", "n")
+vim.keymap.del("n", "N")
+vim.keymap.del("x", "n")
+vim.keymap.del("x", "N")
+
+-- Set 'n' and 'N' to their default behaviors
+vim.api.nvim_set_keymap("n", "n", "n", {})
+vim.api.nvim_set_keymap("n", "N", "N", {})
+vim.api.nvim_set_keymap("x", "n", "n", {})
+vim.api.nvim_set_keymap("x", "N", "N", {})
