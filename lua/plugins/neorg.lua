e828b17a60c58794942c88ab35a62c9d1d5c1c93
imli700 <imlijangba@gmail.com>
2024-12-26 15:36:41 +0530

    Remove outdated neorg workspaces

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -13,12 +13,6 @@ return {
             workspaces = {
               top = "~/Documents/zettelkasten-notes/top/",
               swayam = "~/Documents/zettelkasten-notes/swayam/",
-              NlpAndAi = "~/Documents/zettelkasten-notes/3-semester/nlp-and-ai/",
-              evs = "~/Documents/zettelkasten-notes/3-semester/evs/",
-              dbms = "~/Documents/zettelkasten-notes/3-semester/dbms/",
-              pythonProgramming = "~/Documents/zettelkasten-notes/3-semester/pythong-programming",
-              pythonLab = "~/Documents/zettelkasten-notes/3-semester/python-lab",
-              dataStructure = "~/Documents/zettelkasten-notes/3-semester/data-structure",
               cs50 = "~/Documents/zettelkasten-notes/cs50/",
             },
             default_workspace = "top",
