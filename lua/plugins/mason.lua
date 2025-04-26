13987a51491035d8195e9868e0fc7ff0566ae4b2
imli700 <imlijangba@gmail.com>
2025-04-26 15:33:26 +0530

    Hardcode Treesitter and LSP for HTML and CSS

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+-- lua/plugins/mason.lua
+return {
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    -- Add LSP servers and other tools to ensure installation.
+    -- `vim.list_extend` merges with existing defaults.
+    vim.list_extend(opts.ensure_installed, {
+      "html-lsp", -- LSP server for HTML
+      "css-lsp", -- LSP server for CSS, SCSS, Less
+      "lua-language-server", -- LSP server for Lua
+      -- add any other *always wanted* mason tools here
+    })
+  end,
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+-- lua/plugins/treesitter.lua
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- Add parsers explicitly to ensure installation, even if they are defaults.
+    -- `vim.list_extend` merges with existing defaults.
+    vim.list_extend(opts.ensure_installed, {
+      "html",
+      "css",
+      "lua",
+      -- add any other *always wanted* parsers here
+    })
+  end,
+}
