1c521b87fba228d489a2803a4a1cb19342963d7b
imli700 <imlijangba@gmail.com>
2025-08-14 13:02:06 +0530

    Remove vim-kitty-navigator
    It simply didn't work

--- lua/plugins/vim-kitty-navigator.lua
+++
@@ -1,7 +0,0 @@
--- lua/plugins/kitty-navigator.lua
-return {
-  "knubie/vim-kitty-navigator",
-  -- The `build` key runs this shell command after the plugin is
-  -- installed or updated. This is the key to automation.
-  build = "cp ./*.py ~/.config/kitty/",
-}
