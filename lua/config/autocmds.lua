6a0bc91defcddb16219f7ba25d7ae69462402dcd
imli700 <imlijangba@gmail.com>
2026-02-15 16:18:08 +0530

    Stop indent in every new line after org list entry

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,18 +31,42 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- disable automatic org heading continuation
+-- Combined Org Mode Configuration
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "org",
   callback = function()
+    -- --- PART 1: Your Existing Config (Format Options & Normal Mode) ---
+
     -- 1. Disable flags that auto-create stars on Enter (r), o/O (o), or Wrap (c)
     vim.opt_local.formatoptions:remove({ "r", "o", "c" })
 
     -- 2. Clear what Neovim considers a comment so it doesn't see '*' as a leader
     vim.opt_local.comments = ""
 
-    -- 3. Your manual overrides to force default behavior
+    -- 3. Your manual overrides for Normal Mode
     vim.keymap.set("n", "o", "o", { buffer = true, silent = true })
     vim.keymap.set("n", "O", "O", { buffer = true, silent = true })
+    -- Note: This 'n' map triggers 'i<CR>', which will use the custom insert map below!
     vim.keymap.set("n", "<CR>", "i<CR><Esc>", { buffer = true, silent = true })
+
+    -- --- PART 2: The Fix for List Indentation (Insert Mode) ---
+
+    vim.keymap.set("i", "<CR>", function()
+      local line = vim.api.nvim_get_current_line()
+
+      -- Regex checks if line starts with whitespace + hyphen + space (e.g., "  - ")
+      if line:match("^%s*-%s") then
+        -- It is a List:
+        -- 1. Get the indentation of the hyphen itself (e.g., "  ")
+        local hyphen_indent = line:match("^(%s*)")
+
+        -- 2. Return Newline + CTRL-U (clear auto-indent) + Hyphen Indent
+        return "\r\021" .. hyphen_indent
+      else
+        -- It is a Heading or Paragraph:
+        -- Return standard Enter (keeps the "Smart Indent" you like for headings)
+        return "\r"
+      end
+    end, { buffer = true, expr = true })
   end,
 })
