459b372b6cd90ca7685673224a3361cd24cb82d9
imli700 <imlijangba@gmail.com>
2026-09-12 19:11:14 +0530

    Add transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,1 +1,30 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  -- Tell LazyVim to use catppuccin as the default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  -- Configure Catppuccin to enable transparency
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        -- Ensures floating windows and elements match nicely
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    },
+  },
+}
